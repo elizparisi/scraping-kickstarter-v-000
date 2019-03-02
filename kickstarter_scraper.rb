997ae994a4d6efeea26bbab2aabcc3d8458f9974
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-03-02 04:07:08 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,9 +10,11 @@ def create_project_hash
   projects = {}
   
   kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
     
-    projects[project] = {}
+    projects[title.to_sym] = {}
   end
+  projects
 end
 
 create_project_hash
